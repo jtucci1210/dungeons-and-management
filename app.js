e2d28d17062df3fd3256d6141f77805b08ad7cc2
Jared <jmarkowitz098@gmail.com>
2020-01-20 10:47:58 -0800

    Get express running and connect to Mongo server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
+const db = require('./config/keys').mongoURI;
+const port = process.env.PORT || 5000;
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Goodbye World"));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
