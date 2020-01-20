154456da245ed0d6103ee11b937ebf473541bf1b
Macallan Savett <jsavett@gmail.com>
2020-01-20 15:46:29 -0800

    Delete login import in signup container

--- frontend/src/components/session/signup_form_container.js
+++ frontend/src/components/session/signup_form_container.js
@@ -1,7 +1,6 @@
 import { connect } from 'react-redux';
 import { signup } from '../../actions/session_actions';
 import SignupForm from './signup_form';
-import { login } from '../../actions/session_actions';
 
 const mapStateToProps = (state) => {
     return {
