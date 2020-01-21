a4c876f63c11d11274cc260e581dd86747a428b7
Jared <jmarkowitz098@gmail.com>
2020-01-21 10:22:26 -0800

    Add Character model

--- models/Character.js
+++ models/Character.js
@@ -2,14 +2,70 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CharacterSchema = new Schema({
+    //User Info
     userId: {
-        type: Integer,
+        type: Schema.ObjectId,
         required: true
     },
+
+    //Character Info
     name: {
         type: String,
         required: true
+    },
+    race: {
+        type: String,
+        required: true
+    },
+    subrace: {
+        type: String
+    },
+    class: {
+        type: String,
+        required: true
+    },
+    subclass: {
+        type: String,
+        required: true
+    },
+    armortype: {
+        type: String,
+        required: true
+    },
+
+    //Character Stats
+    level: {
+        type: Number,
+        required: true
+    },
+    maxHp: {
+        type: Number,
+        required: true
+    },
+    currentHp: {
+        type: Number,
+        required: true
+    },
+
+    //Character abilities and skills
+    abilities: {
+        type: {},
+        required: true
+    },
+    skills: {
+        type: {},
+        required: true
+    },
+
+    //Creation and updates logs
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    },
+    dateUpdated: {
+        type: Date
     }
+    
 })
 
 
