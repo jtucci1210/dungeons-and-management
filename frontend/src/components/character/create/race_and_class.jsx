aa1429a710336cdb5cfdee39fd192a6a17da1bf0
Micaela Moss <mcmoss1991@gmail.com>
2020-01-22 09:43:01 -0800

    Create race and class component and stylesheet for it

--- frontend/src/components/character/create/race_and_class.jsx
+++ frontend/src/components/character/create/race_and_class.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../../../stylesheet/race_and_class.css';
+
 
 class RaceAndClass extends React.Component {
     constructor(props) {
@@ -9,7 +11,18 @@ class RaceAndClass extends React.Component {
     render() {
         return (
             <div className="race-and-class-container">
+                <div className="race-and-class">
+                    <div className="race-container">
+                        <h1>Race</h1>
 
+                    </div>
+                    <div className="class-container">
+                        <h1>Class</h1>
+                    </div>
+                </div>
+                <div className="final-stats-container">
+
+                </div>
             </div>
         )
     }
