a9d1909bd56d30cf598f39fe1db8ec16e361e20f
jptucci <jptucci1210@gmail.com>
2020-01-23 09:21:38 -0800

    merge

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -9,8 +9,8 @@ export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
 
 // We'll dispatch this when our user signs in
 export const receiveCurrentUser = currentUser => ({
-    type: RECEIVE_CURRENT_USER,
-    currentUser
+        type: RECEIVE_CURRENT_USER,
+        currentUser
 });
 
 // This will be used to redirect the user to the login page upon signup
