2d37042cedfaf146dfc1f41377d5d40920c0ab8d
Jared <jmarkowitz098@gmail.com>
2020-01-23 09:40:41 -0800

    Prepare for merge

--- app.js
+++ app.js
@@ -23,6 +23,26 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
+//Websockets
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
+
+
+http.listen(8080, function () {
+    console.log('listening on *:8080');
+});
+
+
 //Routes
 app.use("/api/users", users);
 app.use("/api/characters", characters);
