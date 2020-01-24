0ca5ca90e2d358d7b26aba4434cf5ee6756a0d85
Macallan Savett <jsavett@gmail.com>
2020-01-24 08:36:13 -0800

    Resolve conflict on homepage

--- frontend/src/components/homepage/homepage.js
+++ frontend/src/components/homepage/homepage.js
@@ -10,49 +10,6 @@ class HomePage extends React.Component {
         super(props);
     }
 
-<<<<<<< HEAD
-    characterInfo() {
-        const characters = [
-            { id: "5e28927822d5dd01891a7618" , name: 'character1', race: 'woodElf', health: 20, class: 'rogue' },
-            { id: "5e28929522d5dd01891a7619", name: 'character2', race: 'Elf', health: 40, class: 'rogue' },
-        ];
-
-        return (
-            <div className="index-characters">
-                    <Link to={`/characters/new`} className="home-page-create-character">
-                        <div className="home-page-create-character-text"> + </div>
-                    </Link>
-                {characters.map((character, i) => (
-                    <div key={`character-${i}`} className="index-characters-character-info">
-                        <Link to={`/characters/${character.id}`} className='home-characters-link'>
-                        <div className='character-box'>
-                            <div className="character-name-index">
-                                <h3> Character Name: </h3> {character.name}
-                            </div>
-                            <div className="character-image-index">
-                                <div className='character-image'>Img placeholder</div>
-                                <div className="character-image-health-index">
-                                    Health: {character.health}
-                                </div>
-                            </div>
-                            <div className="character-race-index">
-                                <h3>Race: </h3>  {character.race}
-                            </div>
-                            <div className="character-class-index">
-                                <h3>Class: </h3> {character.class}
-                            </div>
-                        </div>
-                        </Link>
-                    </div>
-                ))}
-                  
-            </div>
-        )
-    }
-    
-
-=======
->>>>>>> dev_branch
     render() {
         
         return (
