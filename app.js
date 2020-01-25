d98af02182777484e55d4e7578e73bcb0bc22396
Jared <jmarkowitz098@gmail.com>
2020-01-25 15:00:18 -0800

    Merge dev on top

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ const db = require('./config/keys').mongoURI;
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err));
+        .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log("Connected to MongoDB successfully"))
+        .catch(err => console.log(err));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
@@ -32,35 +32,35 @@ app.use("/api/campaigns", campaigns);
 //Websockets Set-Up
 const socketPort = 8080;
 const server = app.listen(socketPort, function () {
-    console.log("Listening at http://localhost: " + socketPort);
+        console.log("Listening at http://localhost: " + socketPort);
 });
 const io = socketio(server);
 
 //Socket Listeners
 //These listen for events that get sent from the frontend and then send back a response
 io.on("connection", function (socket) {
-    console.log("made connection with socket " + socket.id);
+        console.log("made connection with socket " + socket.id);
 
-    socket.on("joinCampaign", function (roomName) {
-        socket.join(roomName); //Join a room
-        console.log("Joined room: " + roomName)
-        io.to(roomName).emit("receive-room", "made it")
-    });
+        socket.on("joinCampaign", function (roomName) {
+                socket.join(roomName); //Join a room
+                console.log("Joined room: " + roomName)
+                io.to(roomName).emit("receive-room", "made it")
+        });
 
-    //Test listener for a room and responsding
-    // socket.on("test-room", function (data) {
-    //     io.to(data).emit("receive-room", 'more-success')
-    // });
+        //Test listener for a room and responsding
+        // socket.on("test-room", function (data) {
+        //         io.to(data).emit("receive-room", 'more-success')
+        // });
 
-    socket.on("hp", function (data) {
-        io.sockets.emit("hp", data);
-    });
+        socket.on("hp", function (data) {
+                io.sockets.emit("hp", data);
+        });
 
-    socket.on("dice", function (data) {
-        io.sockets.emit("dice", data);
-    });
+        socket.on("dice", function (data) {
+                io.sockets.emit("dice", data);
+        });
 
-    socket.on("total", function (data) {
-        io.sockets.emit("total", data);
-    });
+        socket.on("total", function (data) {
+                io.sockets.emit("total", data);
+        });
 });
--- validation/create-campaign.js
+++ validation/create-campaign.js
@@ -2,20 +2,20 @@ const Validator = require('validator');
 const validText = require('./valid-text');
 
 module.exports = function validateCreateCampInput(data) {
-    let errors = {}
+        let errors = {}
 
-    data.name = validText(data.name) ? data.name : '';
+        data.name = validText(data.name) ? data.name : '';
 
-    // if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
-    //     errors.name = 'Name must be between 2 and 30 characters';
-    // }
+        // if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+        //         errors.name = 'Name must be between 2 and 30 characters';
+        // }
 
-    // if (Validator.isEmpty(data.name)) {
-    //     errors.name = 'Name field is required';
-    // }
+        // if (Validator.isEmpty(data.name)) {
+        //         errors.name = 'Name field is required';
+        // }
 
-    return {
-        errors,
-        isValid: Object.keys(errors).length === 0
-    };
+        return {
+                errors,
+                isValid: Object.keys(errors).length === 0
+        };
 }
