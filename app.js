e745a92a27ac7c723349fe609f8482e4fc80c02f
jptucci <jptucci1210@gmail.com>
2020-01-25 18:02:56 -0800

    Add heroku features

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const socket = require("socket.io");
+const path = require('path');
 
 //File Imports
 const users = require("./routes/api/users");
@@ -15,6 +16,14 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
