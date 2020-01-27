f17bd00bf41d3749dcd120169d61c8a301125353
jptucci <jptucci1210@gmail.com>
2020-01-26 16:15:40 -0800

    Finish styling char show page

--- frontend/src/components/character/show/character_show.js
+++ frontend/src/components/character/show/character_show.js
@@ -4,6 +4,8 @@ import '../../../stylesheet/show_page.css'
 import '../../../stylesheet/test.css'
 import GeneralStats from './character_general_stats';
 import MainStats from './character_main_stats';
+import splashImg from '../../splash/splash_image.jpg'
+
 
 
 
@@ -29,34 +31,26 @@ class CharacterShowPage extends React.Component {
             const character = this.props.character
            
             return (
-                <div className='show-character-box'>
-                    <div className='show-character-header'>
-                        <div className="show-character-name">
-                            {character.name}
-                        </div>
-                        <div className="show-character-hp">
-                            <div className="show-character-current-hp">
-                                Max Life: {character.maxHp}
-                            </div>
-                            <div className="show-character-current-hp">
-                                Current Life: {character.currentHp}
-                            </div>
-                            <div className="show-character-current-hp">
-                                {character.level}d
-                            </div>
-                        </div>
+            <div className="main-page-background-img">
+              <img src={splashImg} alt="background" className="splash-image" />
+              <div className="show-character-page">
+                <div className="show-character-box">
+                  <div className="show-character-header">
+                    <div className="show-character-name">{character.name}</div>
+                    <div className="edit-delete-char">
+                        <button className="edit-delete-char-btn">Edit Character</button>
+                        <button className="edit-delete-char-btn">Delete Character</button>
                     </div>
-                    <div className='show-character-page'>
-                        <div>
-                            <GeneralStats character={character}/>
-                        </div>
-                        <MainStats character={character} />
-                    </div>
-                    <div>
-                        {/* <Items character={character}/> */}
-                    </div>
+                  </div>
+                  <div className="all-stats">
+                    <GeneralStats character={character} />
+                    <MainStats character={character} />
+                  </div>
                 </div>
-            )
+                <div>{/* <Items character={character}/> */}</div>
+              </div>
+            </div>
+            );
         } else {
             return (<div className='test'>Loading</div>)
         }
