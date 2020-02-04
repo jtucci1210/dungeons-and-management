787f38dd62e151add59ff5aa299251e5d52eb072
Micaela Moss <mcmoss1991@gmail.com>
2020-02-04 09:12:44 -0800

    Update number of steps and begin writing them out

--- frontend/src/components/tutorial/tutorial.jsx
+++ frontend/src/components/tutorial/tutorial.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import '../../stylesheet/tutorial.css';
-import characterIndexImg from './1-character-index.png';
 import selectCharacterCreateImg from './2-select-character-create.png';
 import generateDiceRollsImg from './3-generate-dice-rolls.png';
-import rollsGeneratedImg from './4-rolls-generated.png';
 import abilityScoresTooltipImg from './5-ability-scores-tooltip.png';
 import selectAbilityScores from './6-select-ability-scores.png';
 import dontForgetCharName from './7-dont-forget-char-name.png';
-import raceAndClassSelectionSection from './8-race-and-class-selection-section.png';
 import selectRace from './9-select-race.png';
 import selectSubrace from './10-select-subrace.png';
 import selectClass from './11-select-class.png';
@@ -24,42 +21,57 @@ class Tutorial extends React.Component {
         return (
             <div className="tutorial-container">
                 <div className="tutorial-step">
-                    <p className="tutorial-step-description">I am description of step 1</p>
-                        <img 
-                        src={characterIndexImg} 
-                        alt=""
-                        className="tutorial-img"/>
-                </div>
-                <div className="tutorial-step">
-                    <p className="tutorial-step-description">I am description of step 2</p>
+                    <p className="tutorial-step-description">
+                        <strong>Create New Character</strong>
+                        <br/>
+                        <br/>
+                        On the homepage, click on the plus box.
+                        <br />
+                        <br />
+                        It will turn dark green on hover.
+                    </p>
                     <img 
                         src={selectCharacterCreateImg}
                     alt=""
                     className="tutorial-img"/>
                 </div>
                 <div className="tutorial-step">
-                    <p className="tutorial-step-description">I am description of step 3</p>
+                    <p className="tutorial-step-description">
+                        <strong>Generate Dice Rolls</strong>
+                        <br/>
+                        <br/>
+                        Click on the generate button to generate your dice rolls.
+                    </p>
                     <img 
                         src={generateDiceRollsImg}
                     alt=""
                     className="tutorial-img"/>
                 </div>
                 <div className="tutorial-step">
-                    <p className="tutorial-step-description">I am description of step 4</p>
-                    <img 
-                        src={rollsGeneratedImg}
-                    alt=""
-                    className="tutorial-img"/>
-                </div>
-                <div className="tutorial-step">
-                    <p className="tutorial-step-description">I am description of step 5</p>
+                    <p className="tutorial-step-description">
+                        <strong>Select Ability Scores</strong>
+                        <br/>
+                        <br/>
+                        Next you will be able to select which ability score you want associated with each roll.
+                        <br/>
+                        <br/>
+                        You can hover over the '?' icon for more information about each ability.
+                    </p>
                     <img 
                         src={abilityScoresTooltipImg}
                     alt=""
                     className="tutorial-img"/>
                 </div>
                 <div className="tutorial-step">
-                    <p className="tutorial-step-description">I am description of step 6</p>
+                    <p className="tutorial-step-description">
+                        <strong>Ability Scores Cont'd</strong>
+                        <br/>
+                        <br/>
+                        Make sure you have associated each roll with an ability score.
+                        <br/>
+                        <br/>
+                        You cannot have duplicates.
+                    </p>
                     <img 
                         src={selectAbilityScores}
                     alt=""
@@ -69,13 +81,6 @@ class Tutorial extends React.Component {
                     <p className="tutorial-step-description">I am description of step 7</p>
                     <img 
                         src={dontForgetCharName}
-                    alt=""
-                    className="tutorial-img"/>
-                </div>
-                <div className="tutorial-step">
-                    <p className="tutorial-step-description">I am description of step 8</p>
-                    <img 
-                        src={raceAndClassSelectionSection}
                     alt=""
                     className="tutorial-img"/>
                 </div>
