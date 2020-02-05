dd572e5e7a6aabdf0447f90427a865f9f19ee5e1
Micaela Moss <mcmoss1991@gmail.com>
2020-02-05 09:53:25 -0800

    Add campaign errors in action and reducers

--- frontend/src/actions/campaign_actions.js
+++ frontend/src/actions/campaign_actions.js
@@ -1,5 +1,7 @@
 import * as CampaignUtil from "../util/campaign_util";
+import { RECEIVE_CHARACTER_ERRORS } from "./character_actions";
 export const RECEIVE_CAMPAIGN = "RECEIVE_CAMPAIGN";
+export const RECEIVE_CAMPAIGN_ERRORS = "RECEIVE_CAMPAIGN_ERRORS";
 
 const receiveCampaign = payload => {
   return {
@@ -9,6 +11,11 @@ const receiveCampaign = payload => {
     churrentCharId: payload.data.churrentCharId
   };
 };
+
+const receiveCampaignErrors = errors => ({
+  type: RECEIVE_CHARACTER_ERRORS,
+  errors
+});
 
 //Response is not needed so we don't need a thunk action
 export const leaveCampaign = (id, charId) => {
@@ -25,7 +32,10 @@ export const getCampaign = (id) => dispatch => {
 export const createCampaign = () => dispatch => {
   return CampaignUtil.create().then(campaign =>
     dispatch(receiveCampaign(campaign))
-  );
+  )
+  .catch(err => (
+    dispatch(receiveCampaignErrors(err.response.data))
+  ));
 };
 
 //Fetches campaign and add charId to that campaign
